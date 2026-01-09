37f7ca9dbb0317c19afa6ecc9771174407264c0c
fayyoz24 <fayyozusmon@gmail.com>
2026-01-09 15:22:59 +0500

    sd

--- backend/lesson2/classr.py
+++ backend/lesson2/classr.py
@@ -2,5 +2,9 @@
 # github.com
 # python -m venv venv
 # venv\Scripts\activate.bat
-pip install django
+# pip install django
 
+# git add .
+# git commit -m "nima o'zgarish qilindi"
+# git push
+# git config --global user.email "smth@gmail.com"
